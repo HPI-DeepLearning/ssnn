20e5f7076c1140171dd4e376c812e3fbfbe7e798
Joseph Bethge <Jopyth@users.noreply.github.com>
2018-04-05 17:36:43 +0200

    add sigmoid function

--- length/functions/__init__.py
+++ length/functions/__init__.py
@@ -1,5 +1,6 @@
 from .accuracy import accuracy
 from .relu import relu
+from .sigmoid import Sigmoid
 from .softmax import softmax
 from .softmax_cross_entropy import softmax_cross_entropy
 from .sum import sum
