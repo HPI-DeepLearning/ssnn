506f14e5df158dc5ac4078db9894aae81135c37c
Joseph Bethge <Jopyth@users.noreply.github.com>
2018-04-05 14:57:43 +0200

    add fashion mnist test and exclude data set tests from travis

--- length/tests/data_set_tests/test_data_sets.py
+++ length/tests/data_set_tests/test_data_sets.py
@@ -2,8 +2,7 @@ import os
 import shutil
 import pytest
 
-from data_sets import Mnist
-
+from data_sets import Mnist, FashionMnist
 
 temp_folder = ".temp"
 
@@ -20,7 +19,7 @@ def test_mnist_reading_test():
     data_set = Mnist(100)
     iterations = 0
     for batch in data_set.train:
-        assert batch.data.shape == (100, 28, 28)
+        assert batch.data.shape == (100, 784)
         assert batch.labels.shape == (100,)
         iterations += 1
     assert iterations == 600
@@ -30,7 +29,7 @@ def test_mnist_reading_train():
     data_set = Mnist(100)
     iterations = 0
     for batch in data_set.test:
-        assert batch.data.shape == (100, 28, 28)
+        assert batch.data.shape == (100, 784)
         assert batch.labels.shape == (100,)
         iterations += 1
     assert iterations == 100
@@ -38,6 +37,22 @@ def test_mnist_reading_train():
 
 def test_mnist_downloading(cleanup):
     data_set = Mnist(10, delay_loading=True)
+    data_set.path = temp_folder
+    data_set.download_files()
+
+    required_files = [
+        "train-images-idx3-ubyte.gz",
+        "train-labels-idx1-ubyte.gz",
+        "t10k-images-idx3-ubyte.gz",
+        "t10k-labels-idx1-ubyte.gz",
+    ]
+
+    for file in required_files:
+        assert os.path.isfile(os.path.join(temp_folder, file))
+
+
+def test_fashion_mnist_downloading(cleanup):
+    data_set = FashionMnist(10, delay_loading=True)
     data_set.path = temp_folder
     data_set.download_files()
 
