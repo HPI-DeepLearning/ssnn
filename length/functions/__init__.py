8c3d3bdfea753c775058f36f3597bede5b8af238
Joseph Bethge <Jopyth@users.noreply.github.com>
2018-04-06 09:00:20 +0200

    improve pr

--- length/functions/__init__.py
+++ length/functions/__init__.py
@@ -1,6 +1,6 @@
 from .accuracy import accuracy
 from .relu import relu
-from .sigmoid import Sigmoid
+from .sigmoid import sigmoid
 from .softmax import softmax
 from .softmax_cross_entropy import softmax_cross_entropy
 from .sum import sum
--- length/tests/function_tests/test_sigmoid.py
+++ length/tests/function_tests/test_sigmoid.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 from length import constants
-from length.functions import Sigmoid
+from length.functions import sigmoid
+from length.functions.sigmoid import Sigmoid
 from length.graph import Graph
 from length.tests import gradient_checker
 
@@ -9,8 +10,7 @@ from length.tests import gradient_checker
 def test_sigmoid_forward():
     data = np.random.uniform(-1, 1, (3, 2)).astype(constants.DTYPE)
 
-    sigmoid_function = Sigmoid()
-    sigmoid_output = sigmoid_function(Graph(data))
+    sigmoid_output = sigmoid(Graph(data))
     desired = np.divide(np.ones_like(data), np.add(np.ones_like(data), np.exp(np.negative(data))))
     np.testing.assert_allclose(sigmoid_output.data, desired)
 
