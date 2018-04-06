c34966b4fc801c9737669703f289febbfbca4d71
Christian Bartz <christian.bartz@student.hpi.uni-potsdam.de>
2018-04-06 16:14:09 +0200

    increase numerical stability of softmax

--- length/functions/softmax.py
+++ length/functions/softmax.py
@@ -3,6 +3,22 @@ import numpy as np
 from length.function import Function
 
 
+def log_softmax(x):
+    def logsumexp(x):
+        m = x.max(axis=1, keepdims=True)
+        y = x - m
+        np.exp(y, out=y)
+        s = y.sum(axis=1, keepdims=True)
+        np.log(s, out=s)
+        m += s
+        return m
+
+    log_z = logsumexp(x)
+    # log(e^(x)/log_z)
+    y = x - log_z
+    return y
+
+
 class Softmax(Function):
     """
     Abstract Layer is a super class for all neural network layers
@@ -15,9 +31,7 @@ class Softmax(Function):
     def internal_forward(self, inputs):
         x, = inputs
         assert x.ndim == 2, "Softmax only supports two-dimensional input"
-        self.y = x - np.amax(x, axis=1, keepdims=True)
-        np.exp(self.y, out=self.y)
-        self.y /= self.y.sum(axis=1, keepdims=True)
+        self.y = np.exp(log_softmax(x))
         return self.y,
 
     def internal_backward(self, inputs, gradients):
--- length/functions/softmax_cross_entropy.py
+++ length/functions/softmax_cross_entropy.py
@@ -1,26 +1,25 @@
 import numpy as np
 
 from length.function import Function
-from length.functions.softmax import Softmax
+from length.functions.softmax import log_softmax
 
 
 class SoftmaxCrossEntropy(Function):
 
     def __init__(self):
         super().__init__()
-        self.softmax_layer = Softmax()
         self.y = None
 
     def internal_forward(self, inputs):
         x, t = inputs
-        self.y, = self.softmax_layer.internal_forward((x,))
-        loss = -np.log(self.y[range(len(t)), t]).sum(keepdims=True) / t.size
+        self.y = log_softmax(x)
+        loss = -self.y[range(len(t)), t].sum(keepdims=True) / t.size
         return loss,
 
     def internal_backward(self, inputs, gradients):
         x, t = inputs
         grad_in, = gradients
-        grad_x = self.y.copy()
+        grad_x = np.exp(self.y.copy())
         grad_x[range(len(t)), t] -= 1
         grad_x *= grad_in[0] / t.size
         return grad_x, None
