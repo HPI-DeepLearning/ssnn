9824846e1385a95e55d92df0eb5db27d365b3989
Joseph Bethge <Jopyth@users.noreply.github.com>
2018-04-09 16:04:00 +0200

    test for sigmoid, resolves #39

--- length/tests/function_tests/test_sigmoid.py
+++ length/tests/function_tests/test_sigmoid.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from length import constants
+from length.constants import DTYPE
 from length.functions import sigmoid
 from length.functions.sigmoid import Sigmoid
 from length.graph import Graph
@@ -8,10 +9,19 @@ from length.tests import gradient_checker
 
 
 def test_sigmoid_forward():
-    data = np.random.uniform(-1, 1, (3, 2)).astype(constants.DTYPE)
+    data = np.array([
+        [-0.22342056,  0.6927312 ],
+        [ 0.4227562 , -0.59764487],
+        [ 0.7870561 ,  0.372502  ]
+    ], dtype=DTYPE)
 
     sigmoid_output = sigmoid(Graph(data))
-    desired = np.divide(np.ones_like(data), np.add(np.ones_like(data), np.exp(np.negative(data))))
+    desired = np.array([
+        [0.44437608, 0.66657424],
+        [0.6041426 , 0.3548827 ],
+        [0.6871989 , 0.5920634 ]
+    ], dtype=DTYPE)
+
     np.testing.assert_allclose(sigmoid_output.data, desired)
 
 
