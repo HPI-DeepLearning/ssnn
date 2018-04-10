a4c5394434392eeb26c71611fa159037f0e58869
Joseph Bethge <Jopyth@users.noreply.github.com>
2018-04-10 14:29:53 +0200

    prepare fully connected

--- length/layers/fully_connected.py
+++ length/layers/fully_connected.py
@@ -15,31 +15,42 @@ class FullyConnected(Layer):
 
     def __init__(self, num_inputs, num_outputs, weight_init=Xavier()):
         super().__init__()
+        # HINT: some ideas for the implementation can be found here:
+        # https://cs231n.github.io/neural-networks-1/#layers
 
-        self._weights = np.zeros((num_outputs, num_inputs,), dtype=DTYPE)
-        weight_init(self._weights)
-        self.bias = np.zeros((num_outputs,), dtype=DTYPE)
+        # TODO: initialize weights with correct shape, using the weight initializer 'weight_init'
+        self._weights = None
+        # TODO: initialize bias with correct shape and correct initial values
+        self.bias = None
 
     @property
     def weights(self):
-        return self._weights.T
+        # TODO: (if necessary) add code to transform between internal and external representation
+        return self._weights
 
     @weights.setter
     def weights(self, value):
-        self._weights = value.T
+        # TODO: (if necessary) add code to transform between internal and external representation
+        self._weights = value
 
     def internal_forward(self, inputs):
         x, = inputs
-        return np.dot(x, self._weights.T) + self.bias,
+        # TODO: calculate result for this layer
+        # HINT: the expected shape of the result is (usually) not equal to the shape of x
+        result = x
+        return result,
 
     def internal_backward(self, inputs, gradients):
         x, = inputs
         grad_in, = gradients
-        grad_x = np.dot(grad_in, self._weights)
-        grad_w = np.dot(grad_in.T, x)
-        grad_b = np.sum(grad_in, axis=0)
+
+        # TODO: calculate gradients with respect to inputs for this layer
+        grad_x = None
+        grad_w = None
+        grad_b = None
 
         assert grad_x.shape == x.shape
+        # the gradients of the weights should have the same shape as the internal weights array for the test case
         assert grad_w.shape == self._weights.shape
         assert grad_b.shape == self.bias.shape
 
@@ -47,5 +58,6 @@ class FullyConnected(Layer):
 
     def internal_update(self, parameter_deltas):
         delta_w, delta_b = parameter_deltas
-        self._weights -= delta_w
-        self.bias -= delta_b
+        # TODO: apply updates to weights and bias according to deltas from optimizer
+        self._weights = self._weights
+        self.bias = self.bias
